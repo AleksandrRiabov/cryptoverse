c05b02dc33437aed09d2f44f384253b337fbdf93
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2023-01-25 23:04:40 +0000

    Updates

---
+++ precache-manifest.0cf7f46da1706ba488bc07a91c45159d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc4a69c93a928c66a121",
+    "url": "/cryptoverse/static/css/main.2b5d8a69.chunk.css"
+  },
+  {
+    "revision": "cc4a69c93a928c66a121",
+    "url": "/cryptoverse/static/js/main.cc4a69c9.chunk.js"
+  },
+  {
+    "revision": "bba1d855782acfa2ac18",
+    "url": "/cryptoverse/static/css/1.b874340d.chunk.css"
+  },
+  {
+    "revision": "bba1d855782acfa2ac18",
+    "url": "/cryptoverse/static/js/1.bba1d855.chunk.js"
+  },
+  {
+    "revision": "a37b0661f1f4aadecbf2",
+    "url": "/cryptoverse/static/js/runtime~main.a37b0661.js"
+  },
+  {
+    "revision": "885327321794d77a6b6f7ab7fd42d271",
+    "url": "/cryptoverse/static/media/cryptocurrency.88532732.png"
+  },
+  {
+    "revision": "3b4d5e87413a4c89fba3b8ff94f13ef1",
+    "url": "/cryptoverse/index.html"
+  }
+];
